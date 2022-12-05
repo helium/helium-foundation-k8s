ca28819a0badce8fa528123158415e8501fe9da6
Chewing Glass <chewinglass123@gmail.com>
2022-12-05 15:29:26 -0600

    Move to main

--- main.tf
+++ main.tf
@@ -17,3 +17,20 @@ terraform {
     }
   }
 }
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+      tags = {
+        Terraform = "true"
+        Environment = var.env
+      }
+  }
+}
